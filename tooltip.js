851dc3bdae756e95a20de906b0724ad409e78e77
Zhaoxian Hu <huzhx@uci.edu>
2020-12-05 23:08:57 -0800

    added connectedCallback()

--- tooltip.js
+++ tooltip.js
@@ -3,6 +3,12 @@ class Tooltip extends HTMLElement {
     super();
     console.log('created tooltip');
   }
+
+  connectedCallback() {
+    const tooltipIcon = document.createElement('span');
+    tooltipIcon.textContent = '(?)';
+    this.appendChild(tooltipIcon);
+  }
 }
 
 customElements.define('huzhx-tooltip', Tooltip);
