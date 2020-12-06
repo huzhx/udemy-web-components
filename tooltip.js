9c805d41963c9fe4a3456e77a1685d088440e355
Zhaoxian Hu <huzhx@uci.edu>
2020-12-05 23:23:45 -0800

    learned to add event listener

--- tooltip.js
+++ tooltip.js
@@ -2,12 +2,25 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     console.log('created tooltip');
+    this.tooltipContainer_;
   }
 
   connectedCallback() {
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = '(?)';
+    tooltipIcon.addEventListener('mouseenter', this.showTooltip_.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this.hideTooltip_.bind(this));
     this.appendChild(tooltipIcon);
+  }
+
+  showTooltip_() {
+    this.tooltipContainer_ = document.createElement('div');
+    this.tooltipContainer_.textContent = 'This is the tooltip text!';
+    this.appendChild(this.tooltipContainer_);
+  }
+
+  hideTooltip_() {
+    this.removeChild(this.tooltipContainer_);
   }
 }
 
